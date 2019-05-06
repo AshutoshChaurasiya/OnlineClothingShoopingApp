d87ffa35e09b2850c41e37200b657b1a1da7de1b
Ashutosh Chaurasiya <ac.ch503@gmail.com>
2019-05-06 21:20:57 +0545

    app completed

---
+++ app/src/main/java/com/example/onlineclothingshoopingapp/ItemDetailsActivity.java
@@ -0,0 +1,30 @@
+package com.example.onlineclothingshoopingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ItemDetailsActivity extends AppCompatActivity {
+    CircleImageView circleImg;
+    TextView tvName, tvPrice, tvDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+
+        circleImg = findViewById(R.id.imgProfile);
+        tvName= findViewById(R.id.tvName);
+        tvPrice= findViewById(R.id.tvPrice);
+        tvDescription= findViewById(R.id.tvDescription);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null){
+            circleImg.setImageResource(bundle.getInt("image"));
+            tvName.setText("Name: "+bundle.getString("name"));
+            tvPrice.setText("Price: "+bundle.getString("price"));
+            tvDescription.setText(bundle.getString("description"));
+        }
+    }
+}
